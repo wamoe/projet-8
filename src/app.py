4972029def726aeb79c6041775d1c5604cbd4fb3
wamoe <woameur@gmail.com>
2026-01-15 14:58:56 +0100

    add route choice to app.py

--- src/app.py
+++ src/app.py
@@ -76,5 +76,5 @@ def predict():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    # debug=True permet de voir les erreurs détaillées
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
